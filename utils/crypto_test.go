e1031861a25cb773ebfecef78a266a68b38e371a
Sarah Jamie Lewis <sarah@mascherari.press>
2017-11-04 13:31:37 -0700

    Adding basic tests for crypto_utils

---
+++ utils/crypto_test.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestGeneratePrivateKey(t *testing.T) {
+        _, err := GeneratePrivateKey()
+        if err != nil {
+                t.Errorf("Error while generating private key: %v", err)
+        }
+}
+
+func TestLoadPrivateKey(t *testing.T) {
+        _,err := LoadPrivateKeyFromFile("../testing/private_key")
+        if err != nil {
+                t.Errorf("Error while loading private key from file: %v", err)
+        }
+}
